9fbd497d0fd5aad216290ad7826161b59045c64e
Rodrigo Ferreira <rdferreira90@gmail.com>
2019-08-16 10:41:04 -0300

    Criada conexão do modelo do Java com o banco

--- src/main/java/com/rodrigo/starwars/DesafioStarWarsApplication.java
+++ src/main/java/com/rodrigo/starwars/DesafioStarWarsApplication.java
@@ -2,10 +2,17 @@ package com.rodrigo.starwars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DesafioStarWarsApplication {
+public class DesafioStarWarsApplication  extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DesafioStarWarsApplication.class);
+	}
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DesafioStarWarsApplication.class, args);
 				
--- src/main/java/com/rodrigo/starwars/model/Planeta.java
+++ src/main/java/com/rodrigo/starwars/model/Planeta.java
@@ -1,5 +1,49 @@
 package com.rodrigo.starwars.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="PLANETA")
+@Entity
 public class Planeta {
 
+	@Id
+	@Column
+	private String nome;
+	
+	@Column
+	private String clima;
+	
+	@Column
+	private String terreno;
+	
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getClima() {
+		return clima;
+	}
+
+	public void setClima(String clima) {
+		this.clima = clima;
+	}
+
+	public String getTerreno() {
+		return terreno;
+	}
+
+	public void setTerreno(String terreno) {
+		this.terreno = terreno;
+	}
+	
+	
 }
